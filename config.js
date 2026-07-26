a911a9ab0b30389815c55e0547fda6296a404fd8
gr1m0ire-x <maraelizadragomir@gmail.com>
2026-07-26 14:36:15 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 // Use your API URL / Project URL and Publishable key / anon public key.
 // Never use a service_role or secret key here.
 export const SUPABASE_URL = "https://aqbpdktkcdukvxpsxfox.supabase.co";
-export const SUPABASE_ANON_KEY = "sb_publishable_4svlCajdXmfVuvmxX2NdmQ_acKG9zN2";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFxYnBka3RrY2R1a3Z4cHN4Zm94Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUwNDg0ODQsImV4cCI6MjEwMDYyNDQ4NH0.p5vAV7t5lCvRmA7Tnv-dcgNk9IwAvxgcd7ievBTS1iw";
